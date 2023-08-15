3e5268f4c490744213c3996d1af910655fb55100
Nathan MacDiarmid <nathanmacdiarmid@gmail.com>
2023-08-15 12:48:48 -0400

    Fixed contact form and began writing for projects

--- Website/script.js
+++ Website/script.js
@@ -1,14 +1,14 @@
 function sendEmail() {
     Email.send({
         SecureToken: "c87cede3-e262-41af-bc49-b896ef03edee",
-        To : 'nathanmacdiarmid@gmail.com',
-        From : document.getElementById("email").value,
+        To : "nathanmacdiarmid@gmail.com",
+        From : "info@nathanmacdiarmid.com",
         Subject : "Email From Website",
         Body : "Name: " + document.getElementById("name").value
             + "<br> Email: " + document.getElementById("email").value
             + "<br> Subject: " + document.getElementById("subject").value
             + "<br> Message: " + document.getElementById("message").value
     }).then(
-        message => alert(message)
+        alert("Email Sent Successfully!")
     );
 }
