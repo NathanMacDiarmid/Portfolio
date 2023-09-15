6a0e76215b9f7e8662c600665a6969ee2469b396
Nathan MacDiarmid <nathanmacdiarmid@cmail.carleton.ca>
2023-09-15 14:31:19 -0400

    Started hamburger menu

--- Website/scroll.js
+++ Website/scroll.js
@@ -1,4 +1,19 @@
 const toTop = document.querySelector(".to-top");
+const menuBtn = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-navbar");
+
+function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+    window.onscroll = function() {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
 
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 100) {
@@ -6,4 +21,9 @@ window.addEventListener("scroll", () => {
     } else {
         toTop.classList.remove("active");
     }
+});
+
+menuBtn.addEventListener("click", function() {
+    menuBtn.classList.toggle("is-active");
+    mobileMenu.classList.toggle("is-active");
 });
